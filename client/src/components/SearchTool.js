efea85ecb240c38b0277bec5338caee3f77e9035
chrisjmyoon <chris.jm.yoon@gmail.com>
2018-05-19 10:49:51 -0700

    Added search tool

--- client/src/components/SearchTool.js
+++ client/src/components/SearchTool.js
@@ -7,16 +7,49 @@ import { fetchCourselist } from '../actions/panelActions';
 class SearchTool extends Component {
     constructor(props) {
         super(props)
+        this.state = {
+            searchText: '',
+            searchResults: [],
+            currentIndex: -1
+        }
+
+        this.onKeyUp = this.onKeyUp.bind(this);
+        this.onChange = this.onChange.bind(this);
     }
 
     componentWillMount() {
         this.props.fetchCourselist()
     }
 
+    onKeyUp(e) {
+        console.log("Keyup: " + e.keyCode)
+    }
+
+    onChange(e) {
+        console.log(e)
+        this.setState({ ["searchText"]: e.target.value });
+        console.log(e.target.value)
+    }
+
     render() {
         return (
             <div>
-                {this.props.courselist}
+                <input 
+                    type="text" 
+                    placeholder="ENTER YOUR COURSES e.g. CPSC 110" 
+                    onKeyUp={this.onKeyUp}
+                    onChange={this.onChange}
+                />
+                <div className="search-result-wrapper__course">
+                    <div className="search-result__course">
+                        <div className="search-result-head__course">
+                            CPSC
+                        </div>
+                        <div className="search-result-body__course">
+                            110
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
