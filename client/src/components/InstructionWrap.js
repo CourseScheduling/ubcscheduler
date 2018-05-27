230c5bc91b7215504a5bde2f39ce6d47f3dde43f
chrisjmyoon <chris.jm.yoon@gmail.com>
2018-05-27 13:15:38 -0700

    Added expand/collapse functionality

--- client/src/components/InstructionWrap.js
+++ client/src/components/InstructionWrap.js
@@ -3,6 +3,17 @@ import React, { Component } from 'react'
 import '../css/components/instruction-wrap.css';
 
 export default class InstructionWrap extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      expanded: false
+    }
+
+    this.expand = this.expand.bind(this)
+  }
+  expand(e) {
+    this.setState({expanded: !this.state.expanded})
+  }
   render() {
     return (
       <div className="instruction-wrap">
@@ -10,8 +21,10 @@ export default class InstructionWrap extends Component {
             <div className="instruction__type">{this.props.instructionType}</div>
             <div className="instruction__msg">{this.props.instruction}</div>
         </div>
-        {this.props.contentComponent}
-        <div className="instruction__footer">
+        <div className={"instruction-wrap__content " + (this.state.expanded ? "" : "display-none")}>
+          {this.props.contentComponent}
+        </div>        
+        <div className="instruction__footer" onClick={this.expand}>
             Expand
         </div>
       </div>
